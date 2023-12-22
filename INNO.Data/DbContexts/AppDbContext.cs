b95bbec037492c50b3c6e7535ccc84d3046fa388
Azizbek <adkhamovaz26@gmail.com>
2023-12-23 02:56:25 +0500

    pacages added

--- INNO.Data/DbContexts/AppDbContext.cs
+++ INNO.Data/DbContexts/AppDbContext.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace INNO.Data.DbContexts
+﻿namespace INNO.Data.DbContexts;
+public class AppDbContext
 {
-    internal class AppDbContext
-    {
-    }
 }
