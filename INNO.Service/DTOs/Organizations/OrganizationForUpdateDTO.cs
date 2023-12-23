fa3e78a4c4140ede6c7b7735de9fc7a7ba3a1ae2
Azizbek <adkhamovaz26@gmail.com>
2023-12-23 14:41:57 +0500

    soomething

--- INNO.Service/DTOs/Organizations/OrganizationForUpdateDTO.cs
+++ INNO.Service/DTOs/Organizations/OrganizationForUpdateDTO.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace INNO.Service.DTOs.Users
+namespace INNO.Service.DTOs.Organizations
 {
-    internal class UserForCreatingDTO
+    internal class OrganizationForUpdateDTO
     {
     }
 }
