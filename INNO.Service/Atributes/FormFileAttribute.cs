6715add13ad0c867f88a81152ffe000451f8b65f
Sardor Bozorov <bozorovsardor0610@gmail.com>
2023-12-23 15:55:01 -0800

    Attributes, Exceptions, Extantions, has been created

---
+++ INNO.Service/Atributes/FormFileAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace INNO.Service.Atributes;
+
+public class FormFileAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        if (value is IFormFile file)
+        {
+            string[] extensions = new string[] { ".png", ".jpg", ".mp3", ".mp4", ".mkv", ".pptx", ".ppt", ".doc", ".docx" };
+            var extension = Path.GetExtension(file.FileName);
+
+            if (!extensions.Contains(extension.ToLower()))
+            {
+                return new ValidationResult("This photo extension is not allowed!");
+            }
+        }
+        return ValidationResult.Success;
+    }
+}
---
+++ INNO.Service/Atributes/UserPassword.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace INNO.Service.Atributes;
+
+public class UserPassword : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        if (value is string password &&
+
+            password.Length >= 8 &&
+            password.Any(c => char.IsDigit(c)) &&
+            password.Any(c => char.IsLetter(c)))
+        {
+
+            return ValidationResult.Success;
+        }
+        return new ValidationResult("Password should contain at least 8 characters," +
+                                                " should contain at least on letter and digit");
+    }
+}
